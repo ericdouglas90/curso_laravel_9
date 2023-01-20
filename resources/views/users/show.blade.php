53d7b95b2bda5b659f5e448ff1d14620f1e5b201
ensilva <ensilva@pdmfc.com>
2023-01-20 12:25:10 -0300

    add back button

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -14,6 +14,7 @@
     @method('DELETE')
     @csrf
     <button type="submit" class="rounded-full bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4">Deletar</button>
+    <a href="{{ route('users.index') }}" class="py-2 px-4 bg-green-500 rounded-full text-white font-bold inline-block w-20 text-center">Back</a>
 </form>
 
 @endsection
